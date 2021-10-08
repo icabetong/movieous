4d4e134af4832add18a9bb452f1430f34cb10b33
Isaiah Collins Abetong <isaiahcollins_02@live.com>
2021-10-08 12:28:27 +0800

    Moved API Key to Code

--- src/infrastructure/Movie.js
+++ src/infrastructure/Movie.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = "fe1f989cea3755852f360fd21faf4a4d";
 export const api = axios.create({ baseURL: BASE_URL });
 export const IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
 
 export const fetch = async (page) => {
     const response = await api.get("/movie/popular/", { params: {
-        api_key: process.env.REACT_APP_MOVIE_API_KEY,
+        api_key: API_KEY,
         page: page
     }});
 
